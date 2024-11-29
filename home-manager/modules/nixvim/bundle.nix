24c67ca2adcf1f3d9b6cca59a0a8e993fbcebd4b
dbozbay <dannybozbay@gmail.com>
2024-11-29 22:21:59 +0000

    basic autocomple

--- home-manager/modules/nixvim/bundle.nix
+++ home-manager/modules/nixvim/bundle.nix
@@ -2,6 +2,45 @@
   programs.nixvim = {
     enable = true;
     defaultEditor = true;
-    colorschemes.catppuccin.enable = true;
+
+    # colorscheme
+    colorschemes.catppuccin = {
+      enable = true;
+      settings = {
+        flavour = "mocha";
+        transparent_background = true;
+        term_colors = true;
+      };
+    };
+
+    # Lualina
+    plugins.lualine = {
+      enable = true;
+    };
+
+    # Lsp
+    plugins.lsp = {
+      enable = true;
+      servers = {
+        nixd.enable = true;
+        lua-ls.enable = true;
+	ruff.enable = true;
+	pyright.enable = true;
+      };
+    };
+
+    # Autocompletion
+    plugins.cmp = {
+      enable = true;
+      autoEnableSources = true;
+      settings = {
+        sources = [
+	  {name = "nvim_lsp";}
+	  {name = "luasnip";}
+	  {name = "path";}
+	  {name = "buffer";}
+        ];
+      };
+    };
   };
 }
